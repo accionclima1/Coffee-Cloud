51602ddb3a42627099fb0e018d18eabf742a86a2
Augusto Valerio <auguvalerio@gmail.com>
2016-09-19 01:22:42 -0600

    admin updates

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ require('./models/Methods');
 require('./models/Messages');
 require('./models/Roya');
 require('./config/passport');
-mongoose.connect('mongodb://cafenube:Sec03lP1nt0@ec2-54-68-110-187.us-west-2.compute.amazonaws.com/dummyDB');
+mongoose.connect('mongodb://cafenube:Sec03lP1nt0@coffeecloud.centroclima.org/dummyDB');
 //WC5OBLfIbWNj
 //{
 //      user: "admin",
