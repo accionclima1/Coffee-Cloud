ffbee3efe7ecb938c3b3f3c08994176e4bf142f3
Augusto Valerio <auguvalerio@gmail.com>
2016-11-01 12:25:43 -0600

    db updates

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ require('./models/Methods');
 require('./models/Messages');
 require('./models/Roya');
 require('./config/passport');
-mongoose.connect('mongodb://root:XR7vwshDKmKW@ec2-35-162-54-166.us-west-2.compute.amazonaws.com/local');
-//WC5OBLfIbWNj
+mongoose.connect('mongodb://cafenube:Sec03lP1nt0@ec2-35-162-54-166.us-west-2.compute.amazonaws.com/dummyDB');
+//XR7vwshDKmKW
 //{
 //      user: "admin",
 //      pwd: "Sec03lP1nt0",
