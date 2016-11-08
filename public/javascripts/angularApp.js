ee39109073dff0a241da955ff7497b8bb654560d
Augusto Valerio <auguvalerio@gmail.com>
2016-11-08 14:02:03 -0600

    updates

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -18,19 +18,7 @@ app.controller('MainCtrl',['$scope','posts', 'auth',
 function($scope, posts, auth){
 	$scope.isLoggedIn = auth.isLoggedIn;
 	$scope.currentUser = auth.currentUser;
-	$scope.posts = posts.posts;
-  	$scope.addPost = function(){
-	  	if(!$scope.title || $scope.title === '') { return; }
-		posts.create({
-		    title: $scope.title,
-		    link: $scope.link,
-		  });
-		$scope.title = '';
-		$scope.link = '';
-	};
-	$scope.incrementUpvotes = function(post) {
-	  posts.upvote(post);
-	};
+	
 	
 }]);
 app.controller('PostsCtrl', [
