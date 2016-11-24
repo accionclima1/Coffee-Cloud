33f8076af976ff958502e09f76ed8220e42666a3
Augusto Valerio <auguvalerio@gmail.com>
2016-11-24 17:57:06 -0600

    cambios

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -2,14 +2,19 @@ var app = angular.module('coffeeScript', ['btford.socket-io','ui.router','snap',
 
 app.config(['localStorageServiceProvider', function(localStorageServiceProvider){
   localStorageServiceProvider.setPrefix('ls');
-}])
+}]);
+
+app.config(function($httpProvider) {
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+});
 
 // Socket Factory service
 app.factory('socket', ['socketFactory',
     function(socketFactory) {
         return socketFactory({
             prefix: '',
-            ioSocket: io.connect('http://coffeecloud.centroclima.org:3000')
+            ioSocket: io.connect('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000')
         });
     }
 ]);
@@ -1183,19 +1188,19 @@ app.factory('posts', ['$http', 'auth', function($http, auth){
 	  		posts : []
 	  };
 	  o.getAll = function() {
-	    return $http.get('/posts').success(function(data){
+	    return $http.get('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/posts').success(function(data){
 	      angular.copy(data, o.posts);
 	    });
 	  };
 	  o.create = function(post) {
-		  return $http.post('/posts', post, {
+		  return $http.post('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/posts', post, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   }).success(function(data){
 		    o.posts.push(data);
 		  });
 		};
 		o.upvote = function(post) {
-		  return $http.put('/posts/' + post._id + '/upvote', null, {
+		  return $http.put('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/posts/' + post._id + '/upvote', null, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   })
 		    .success(function(data){
@@ -1203,17 +1208,17 @@ app.factory('posts', ['$http', 'auth', function($http, auth){
 		    });
 		};
 		o.get = function(id) {
-		  return $http.get('/posts/' + id).then(function(res){
+		  return $http.get('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/posts/' + id).then(function(res){
 		    return res.data;
 		  });
 		};
 		o.addComment = function(id, comment) {
-		  return $http.post('/posts/' + id + '/comments', comment, {
+		  return $http.post('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/posts/' + id + '/comments', comment, {
 		    headers: {Authorization: 'Bearer '+auth.getToken()}
 		  });
 		};
 		o.upvoteComment = function(post, comment) {
-		  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+		  return $http.put('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   })
 		    .success(function(data){
@@ -1234,20 +1239,20 @@ app.factory('user', ['$http', 'auth', function($http, auth){
 		  });
 		};*/
 		o.getAll = function() {
-		  return $http.get('/users', {
+		  return $http.get('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/users', {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   }).then(function(res){
 		    return res.data;
 		  });
 		};
 		o.get = function(id) {
-		  return $http.get('/users/' + id).then(function(res){
+		  return $http.get('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/users/' + id).then(function(res){
 		    return res.data;
 		  });
 		};
 		
 		o.update = function(user){
-	  return $http.put('/users/' + user._id, user, {
+	  return $http.put('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/users/' + user._id, user, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   }).success(function(data){
 	    return data
@@ -1309,13 +1314,13 @@ app.factory('auth', ['$http', '$window', function($http, $window){
 	};
 
 	auth.register = function(user){
-	  return $http.post('/register', user).success(function(data){
+	  return $http.post('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/register', user).success(function(data){
 	    auth.saveToken(data.token);
 	  });
 	};
 
 	auth.logIn = function(user){
-	  return $http.post('/login', user).success(function(data){
+	  return $http.post('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/login', user).success(function(data){
 	    auth.saveToken(data.token);
 	  });
 	};
@@ -1330,18 +1335,18 @@ app.factory('auth', ['$http', '$window', function($http, $window){
 app.factory('unit', ['$http', 'auth','$window', function($http, auth, $window){
    var o = {};
    o.getAll = function(id) {
-	    return $http.get('/users/'+ id +'/units').success(function(data){
+	    return $http.get('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/users/'+ id +'/units').success(function(data){
 	      return data;
 	    });
 	  };
    o.get = function(userId,id) {
-		  return $http.get('/users/'+ userId +'/units/'+ id).then(function(res){
+		  return $http.get('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/users/'+ userId +'/units/'+ id).then(function(res){
 		    return res.data;
 		  });
 		};
    
 	o.create = function(unit, id){
-	  return $http.post('/users/'+ id +'/units', unit, {
+	  return $http.post('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/users/'+ id +'/units', unit, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   }).success(function(data){
 		    return data;
@@ -1349,7 +1354,7 @@ app.factory('unit', ['$http', 'auth','$window', function($http, auth, $window){
 	};
 	
 	o.update = function(unit, id, unitData){
-	  return $http.put('/users/'+ id +'/units/'+ unit, unitData, {
+	  return $http.put('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/users/'+ id +'/units/'+ unit, unitData, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   }).success(function(data){
 	    return data
@@ -1357,7 +1362,7 @@ app.factory('unit', ['$http', 'auth','$window', function($http, auth, $window){
 	};
 	
 	o.deleteUnit = function(unitId, userId){
-	  return $http.delete('/users/'+ userId +'/units/'+ unitId, {
+	  return $http.delete('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/users/'+ userId +'/units/'+ unitId, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   }).success(function(data){
 		    return unitId;
@@ -1372,19 +1377,19 @@ app.factory('methods', ['$http', 'auth', function($http, auth){
 	  		chats : []
 	  };
 	  o.get = function() {
-	    return $http.get('/admin/methods/').success(function(data){
+	    return $http.get('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/admin/methods/').success(function(data){
 	      return data;
 	    });
 	  };
 	  o.create = function(method) {
-		  return $http.post('/admin/methods', method, {
+		  return $http.post('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/admin/methods', method, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   }).success(function(data){
 		    return data;
 		  });
 		};
 		o.update = function(method) {
-		  return $http.put('/admin/methods', method, {
+		  return $http.put('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/admin/methods', method, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   }).success(function(data){
 		    return data;
@@ -1400,19 +1405,19 @@ app.factory('campoService', ['$http', 'auth', function ($http, auth) {
         chats: []
     };
     o.get = function () {
-        return $http.get('/admin/campo/').success(function (data) {
+        return $http.get('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/admin/campo/').success(function (data) {
             return data;
         });
     };
     o.create = function (method) {
-        return $http.post('/admin/campo', method, {
+        return $http.post('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/admin/campo', method, {
             headers: { Authorization: 'Bearer ' + auth.getToken() }
         }).success(function (data) {
             return data;
         });
     };
     o.update = function (method) {
-        return $http.put('/admin/methods', method, {
+        return $http.put('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/admin/methods', method, {
             headers: { Authorization: 'Bearer ' + auth.getToken() }
         }).success(function (data) {
             return data;
@@ -1427,12 +1432,12 @@ app.factory('roya', ['$http', 'auth', function($http, auth){
 	  		
 	  };
 	  o.getAll = function() {
-	    return $http.get('/roya').success(function(data){
+	    return $http.get('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/roya').success(function(data){
 	      return data;
 	    });
 	  };
 	  o.create = function(roya) {
-		 return $http.post('/roya', roya, {
+		 return $http.post('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/roya', roya, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   }).success(function(data){
 		    return data;	
@@ -1451,12 +1456,12 @@ app.factory('gallo', ['$http', 'auth', function($http, auth){
 	  		
 	  };
 	  o.getAll = function() {
-	    return $http.get('/gallo').success(function(data){
+	    return $http.get('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/gallo').success(function(data){
 	      return data;
 	    });
 	  };
 	  o.create = function(gallo) {
-		 return $http.post('/gallo', gallo, {
+		 return $http.post('http://ec2-35-162-54-166.us-west-2.compute.amazonaws.com:3000/gallo', gallo, {
     headers: {Authorization: 'Bearer '+auth.getToken()}
   }).success(function(data){
 		    return data;	
