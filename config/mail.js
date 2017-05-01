2fab72f273e1f6e147dd656ed302b50c6502ed05
Augusto Valerio <auguvalerio@gmail.com>
2017-05-01 09:44:51 -0600

    email update

--- config/mail.js
+++ config/mail.js
@@ -4,7 +4,7 @@ var smtpTransport =  require('nodemailer-smtp-transport');
 var configuration = {
     smtp_host: "smtp.gmail.com",
     smtp_user: "centroclimaorg@gmail.com", // your gmail id
-    smtp_password: "Secoelpinto123!", // your gmail password
+    smtp_password: "Clima3!$", // your gmail password
     mailadmin: 'centroclimaorg@gmail.com'
   };
 var config = configuration
